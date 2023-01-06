1ae04b990be5fa71118cd80555d6c774823fe506
Vince Iannelli <viannelli@gmail.com>
2023-01-06 09:51:11 -0700

    feat:modal,play/pause button, cloud storage source

---
+++ green-eggs-app/components/AudioPlayer.jsx
@@ -0,0 +1,102 @@
+import { View, Text, StyleSheet, Modal, StatusBar, TouchableWithoutFeedback } from 'react-native';
+import { Audio } from 'expo-av';
+import { Button, IconButton, MD3Colors } from 'react-native-paper';
+import React, { useEffect, useState } from 'react';
+
+// Modal is visible or not
+
+const AudioPlayer = ({ visible }) => {
+	const [sound, setSound] = useState();
+	const [button, setButton] = useState('play-circle');
+	const [playing, setPlaying] = useState(false);
+
+	async function playSound() {
+		console.log('Loading Sound');
+		const { sound } = await Audio.Sound.createAsync(
+			{
+				uri: 'https://firebasestorage.googleapis.com/v0/b/hello-calgary-86156.appspot.com/o/testAudio.mp3?alt=media&token=205f5509-c396-4fae-a174-c40f7c587efd',
+			},
+			{ shouldPlay: true }
+		);
+		setSound(sound);
+
+		console.log('Playing Sound');
+		await sound.playAsync();
+	}
+
+	async function pauseSound() {
+		console.log('Pausing Sound');
+		await sound.pauseAsync();
+	}
+
+	useEffect(() => {
+		return sound
+			? () => {
+					console.log('Unloading Sound');
+					sound.unloadAsync();
+			  }
+			: undefined;
+	}, [sound]);
+
+	return (
+		<>
+			<StatusBar hidden />
+			<Modal animationType="slide" transparent visible={visible}>
+				<View style={styles.modal}>
+					<Text style={styles.title} numberOfLines={2}>
+						Egg Title. this is a longer style of tiltejkhsakdhkaj asdkjashdk
+					</Text>
+					<View>
+						<Text style={styles.subtitle}>subtitle</Text>
+					</View>
+					<IconButton
+						icon={button}
+						iconColor={MD3Colors.Neutral10}
+						mode={'contained-tonal'}
+						containerColor={'#ffffff'}
+						size={35}
+						onPress={() => {
+							if (playing === false) {
+								setPlaying(true);
+								setButton('pause-circle');
+								playSound();
+							} else {
+								setButton('play-circle');
+								setPlaying(false);
+								pauseSound();
+							}
+						}}
+					/>
+				</View>
+			</Modal>
+		</>
+	);
+};
+
+const styles = StyleSheet.create({
+	modal: {
+		position: 'absolute',
+		bottom: 0,
+		right: 0,
+		left: 0,
+		backgroundColor: '#fff',
+		borderTopRightRadius: 20,
+		borderTopLeftRadius: 20,
+		zIndex: 1000,
+	},
+	title: {
+		fontSize: 18,
+		fontWeight: 'bold',
+		padding: 20,
+		paddingBottom: 0,
+	},
+	subtitle: {
+		fontSize: 16,
+		fontWeight: 'medium',
+		paddingVertical: 10,
+		padding: 20,
+		letterSpacing: 1,
+	},
+});
+
+export default AudioPlayer;
--- green-eggs-app/components/MapButton.jsx
+++ green-eggs-app/components/MapButton.jsx
@@ -2,14 +2,14 @@ import * as React from 'react';
 import { IconButton, MD3Colors } from 'react-native-paper';
 
 const MapButton = () => (
-  <IconButton
-    icon="camera"
-    iconColor={MD3Colors.Neutral10}
-    mode={'contained-tonal'}
-    containerColor={'#ffffff'}
-    size={35}
-    onPress={() => console.log('Pressed')}
-  />
+	<IconButton
+		icon="play-circle"
+		iconColor={MD3Colors.Neutral10}
+		mode={'contained-tonal'}
+		containerColor={'#ffffff'}
+		size={35}
+		onPress={() => console.log('Pressed')}
+	/>
 );
 
 export default MapButton;
