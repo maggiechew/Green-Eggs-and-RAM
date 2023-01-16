00faee2166d22408335b8ad3bfabda6b16e00571
Maggie Chew <105380332+maggiechew@users.noreply.github.com>
2023-01-15 22:58:02 -0700

    Styling

--- green-eggs-app/screens/ContentScreen.jsx
+++ green-eggs-app/screens/ContentScreen.jsx
@@ -19,87 +19,114 @@ const ContentScreen = () => {
   return (
     <View style={styles.background}>
       <ScrollView style={styles.container}>
-        <Card mode="elevated">
-          <Card.Title
-            title="COOL EGG NAME"
-            subtitle="@CoolEggPerson"
-            subtitleNumberOfLines={2}
-            left={(props) => (
-              <Avatar.Image {...props} source={require('../crazy-chick.jpg')} />
-            )}
-          />
-          <Card.Content>
-            <Divider />
-            <Text variant="bodyMedium" style={styles.shortDescription}>
-              Here's a short descriptor of this egg. Blah blah blah. Doop doop
-              doop. I'm a good egg.
-              Here's a short descriptor of this egg. Blah blah blah. Doop doop
-              doop. I'm a good egg.
-            </Text>
-            {/* <Text style={styles.link}> </Text> */}
-            <Divider />
-          <View style={styles.buttons}>
-          <Card.Actions style={styles.buttons}>
-          <Button onPress={() => {console.log('Loved it')}}>Love</Button>
-          <Button onPress={() => {console.log('Saved it')}}>Save</Button>
-          <Button onPress={() => {console.log('Shared it')}}>Share</Button>
-          <Button onPress={() => {console.log('Reported it')}}>Report</Button>
-        </Card.Actions>
-        </View>
-          </Card.Content>
-        </Card>
-        <Card>
-          <Card.Cover source={require('../Content-Photo.jpg')} />
-          <Card.Content>
-            <List.Section>
-              <List.Accordion
-                title="Learn More"
-              >
-                <SegmentedButtons
-                  value={value}
-                  onValueChange={setValue}
-                  buttons={[
-                    {
-                      value: 'The Piece',
-                      label: 'More about the piece'
-                    },
-                    {
-                      value: 'The Artist',
-                      label: 'More about the Artist'
-                    }
-                  ]}
+        <View style={styles.card}>
+          <Card mode="elevated">
+            <Card.Title
+              title="COOL EGG NAME"
+              subtitle="@CoolEggPerson"
+              subtitleNumberOfLines={2}
+              left={(props) => (
+                <Avatar.Image
+                  {...props}
+                  source={require('../crazy-chick.jpg')}
                 />
-                <List.Item title={value} />
-                {value === 'The Piece' && (
-                    <>
-
-                <Text>Five little eggies</Text>
-                <Text>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
-</Text>
-                    </>
-                
-                )}
-                {value === 'The Artist' && (
-                  <>
-                    <Text>Links here</Text>
-                    <Text>The woman, the legend The woman, the legend</Text>
-                    <Text>
-                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                      Duis aute irure dolor in reprehenderit in voluptate velit
-                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                      sint occaecat cupidatat non proident, sunt in culpa qui
-                      officia deserunt mollit anim id est laborum."
-                    </Text>
-                  </>
-                )}
-              </List.Accordion>
-            </List.Section>
-          </Card.Content>
-        </Card>
+              )}
+            />
+            <Card.Content>
+              <Divider />
+              <Text variant="bodyMedium" style={styles.shortDescription}>
+                Here's a short descriptor of this egg. Blah blah blah. Doop doop
+                doop. I'm a good egg. Here's a short descriptor of this egg.
+                Blah blah blah. Doop doop doop. I'm a good egg.
+              </Text>
+              {/* <Text style={styles.link}> </Text> */}
+              <Divider />
+              <Card.Cover source={require('../Content-Photo.jpg')} />
+              <View style={styles.buttons}>
+                <Card.Actions style={styles.buttons}>
+                  <Button
+                    onPress={() => {
+                      console.log('Loved it');
+                    }}
+                  >
+                    Love
+                  </Button>
+                  <Button
+                    onPress={() => {
+                      console.log('Saved it');
+                    }}
+                  >
+                    Save
+                  </Button>
+                  <Button
+                    onPress={() => {
+                      console.log('Shared it');
+                    }}
+                  >
+                    Share
+                  </Button>
+                  <Button
+                    onPress={() => {
+                      console.log('Reported it');
+                    }}
+                  >
+                    Report
+                  </Button>
+                </Card.Actions>
+              </View>
+            </Card.Content>
+          </Card>
+          <List.Section>
+            <List.Accordion title="Learn More">
+              <SegmentedButtons
+                value={value}
+                onValueChange={setValue}
+                buttons={[
+                  {
+                    value: 'The Piece',
+                    label: 'More about the piece'
+                  },
+                  {
+                    value: 'The Artist',
+                    label: 'More about the Artist'
+                  }
+                ]}
+              />
+              <List.Item title={value} />
+              {value === 'The Piece' && (
+                <>
+                  <Text>Five little eggies</Text>
+                  <Text>
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum."
+                  </Text>
+                </>
+              )}
+              {value === 'The Artist' && (
+                <>
+                  <Text>Links here</Text>
+                  <Text>The woman, the legend The woman, the legend</Text>
+                  <Text>
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum."
+                  </Text>
+                </>
+              )}
+            </List.Accordion>
+          </List.Section>
+        </View>
       </ScrollView>
       <Card style={styles.subcontent}>
         <Card mode="contained" style={styles.sampleAudio}>
@@ -114,23 +141,31 @@ const ContentScreen = () => {
 
 const styles = StyleSheet.create({
   container: {
+    height: '80%',
+  },
+  card: {
+    // height: 2/3
+    height: '100%'
   },
   subcontent: {
+    height: '20%',
   },
   sampleAudio: {
+    // height: 2/3
+    height: '75%'
   },
   background: {
-      flex:1,
-      alignContent: 'space-between'
-    },
-    buttons: {
-        flex:1,
-        alignItems:'center',
-    },
-    shortDescription: {
-        paddingVertical: 10,
-        paddingTop: 15,
-    }
+    height: '100%',
+    alignContent: 'space-between'
+  },
+  buttons: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  shortDescription: {
+    paddingVertical: 10,
+    paddingTop: 15
+  }
 });
 
 export default ContentScreen;
