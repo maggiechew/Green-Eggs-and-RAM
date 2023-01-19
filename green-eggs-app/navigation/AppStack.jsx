74d705380b23577e4628f6467882851e30c6296d
yanliu1111 <liuyan0504@gmail.com>
2023-01-18 20:08:17 -0700

    logout functionality

--- green-eggs-app/navigation/AppStack.jsx
+++ green-eggs-app/navigation/AppStack.jsx
@@ -1,80 +1,40 @@
 import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import IconButton from '../components/IconButton';
-
+import { Button } from 'react-native-paper';
 import { MapPage } from '../screens';
-import { signOut } from 'firebase/auth';
-import { auth } from '../config';
 import { AccountScreen } from '../screens/AccountScreen';
 import { FriendsScreen } from '../screens/FriendsScreen';
 import { ProfileScreen } from '../screens/ProfileScreen';
-
+import { useNavigation } from '@react-navigation/native';
 const Stack = createStackNavigator();
-const handleLogout = () => {
-  signOut(auth).catch((error) => console.log('Error logging out: ', error));
-};
+
 export const AppStack = () => {
+  const navigation = useNavigation();
   return (
     <Stack.Navigator
-      initialRouteName="Login"
-      // screenOptions={{ headerShown: false }}
+      initialRouteName='Login'
+      // screenOptions={{ headerShown: true }}
     >
+      <Stack.Screen name='Map' component={MapPage} />
+      <Stack.Screen name='Account' component={AccountScreen} />
       <Stack.Screen
-        name="Map"
-        component={MapPage}
-        options={{
-          headerRight: ({ tintColor }) => (
-            <IconButton
-              icon="exit"
-              color={tintColor}
-              size={24}
-              onPress={handleLogout}
-            />
-          )
-        }}
-      />
-      <Stack.Screen
-        name="Account"
-        component={AccountScreen}
-        options={{
-          headerRight: ({ tintColor }) => (
-            <IconButton
-              icon="exit"
-              color={tintColor}
-              size={24}
-              onPress={handleLogout}
-            />
-          )
-        }}
-      />
-      <Stack.Screen
-        name="Friends"
+        name='Friends'
         component={FriendsScreen}
         options={{
-          headerRight: ({ tintColor }) => (
-            <IconButton
-              icon="exit"
-              color={tintColor}
-              size={24}
-              onPress={handleLogout}
-            />
+          headerTintColor: 'red',
+          headerStyle: { backgroundColor: 'blue' },
+          headerLeft: () => (
+            <Button
+              style={{ marginLeft: 20 }}
+              onPress={() => navigation.goBack()}
+            >
+              Back
+            </Button>
           )
         }}
       />
-      <Stack.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{
-          headerRight: ({ tintColor }) => (
-            <IconButton
-              icon="exit"
-              color={tintColor}
-              size={24}
-              onPress={handleLogout}
-            />
-          )
-        }}
-      />
+      <Stack.Screen name='Profile' component={ProfileScreen} />
     </Stack.Navigator>
   );
 };
