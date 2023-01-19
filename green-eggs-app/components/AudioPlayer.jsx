c54050ba7544db9f6a4bcad2b7940d5dcb79534e
Vince Iannelli <viannelli@gmail.com>
2023-01-19 16:25:07 -0700

    functioning seek bar

--- green-eggs-app/components/AudioPlayer.jsx
+++ green-eggs-app/components/AudioPlayer.jsx
@@ -1,23 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import {
-  ActivityIndicator,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  View,
-  Text
-} from 'react-native';
+import { StyleSheet, View, Text, Dimensions } from 'react-native';
 import { Audio } from 'expo-av';
 import { Button } from './Button';
-
-// const egg = {
-//   uri: 'https://firebasestorage.googleapis.com/v0/b/hello-calgary-86156.appspot.com/o/testAudio.mp3?alt=media&token=205f5509-c396-4fae-a174-c40f7c587efd',
-//   eggName: 'egg name is cool!'
-// };
+import Slider from '@react-native-community/slider';
 
 const AudioPlayer = ({ egg }) => {
   const [isPlayerReady, setIsPlayerReady] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [duration, setDuration] = useState(undefined);
+  const [position, setPosition] = useState(0);
   const [sound, setSound] = useState(undefined);
   useEffect(() => {
     loadAudio(egg);
@@ -30,11 +21,12 @@ const AudioPlayer = ({ egg }) => {
     );
     setSound(sound);
     sound.setOnPlaybackStatusUpdate((status) => {
-      if (status.isLoaded) setIsPlayerReady(true);
-
-      console.log('STATUS: ', isPlayerReady);
-      // display eggName
-      // display time left
+      if (status.isLoaded) {
+        setIsPlayerReady(true);
+        console.log('STATUS: ', isPlayerReady);
+        setDuration(status.durationMillis);
+        setPosition(1);
+      }
     });
   }
 
@@ -47,12 +39,90 @@ const AudioPlayer = ({ egg }) => {
     if (isPlayerReady && isPlaying) await sound.pauseAsync();
   }
 
-  // async function seekAudio() {}
+  function calculateSeekBar() {
+    if (isPlayerReady && position) {
+      seekAudio();
+      return position / duration;
+    }
+
+    // if (currentAudio.lastPosition) {
+    //   return currentAudio.lastPosition / (currentAudio.duration * 1000);
+    // }
+
+    return 0;
+  }
+
+  async function seekAudio() {
+    sound.setOnPlaybackStatusUpdate((status) => {
+      if (isPlayerReady) setPosition(status.positionMillis);
+      // console.log(status.positionMillis);
+    });
+    return position / duration;
+  }
+
+  const convertTime = (minutes) => {
+    if (minutes) {
+      const hrs = minutes / 60;
+      const minute = hrs.toString().split('.')[0];
+      const percent = parseInt(hrs.toString().split('.')[1].slice(0, 2));
+      const sec = Math.ceil((60 * percent) / 100);
+      if (parseInt(minute) < 10 && sec < 10) {
+        return `0${minute}:0${sec}`;
+      }
+
+      if (sec == 60) {
+        return `${minute + 1}:00`;
+      }
+
+      if (parseInt(minute) < 10) {
+        return `0${minute}:${sec}`;
+      }
+
+      if (sec < 10) {
+        return `${minute}:0${sec}`;
+      }
+
+      return `${minute}:${sec}`;
+    }
+  };
+
+  const renderCurrentTime = () => {
+    // if (!context.soundObj && currentAudio.lastPosition) {
+    //   return convertTime(currentAudio.lastPosition / 1000);
+    // }
+    return convertTime(position / 1000);
+  };
 
   return (
     <View>
       <Text>status: {isPlayerReady} </Text>
       <Text>name: {egg.eggName} </Text>
+      <Text>duration: {duration} </Text>
+      <Text>position: {renderCurrentTime()} </Text>
+      <Slider
+        style={{ width: 200, height: 40 }}
+        minimumValue={0}
+        maximumValue={1}
+        value={calculateSeekBar()}
+        // minimumTrackTintColor={color.FONT_MEDIUM}
+        // maximumTrackTintColor={color.ACTIVE_BG}
+        onValueChange={(value) => {
+          setPosition(value * duration);
+        }}
+        // onSlidingStart={async () => {
+        //   if (!context.isPlaying) return;
+
+        //   try {
+        //     await pause(context.playbackObj);
+        //   } catch (error) {
+        //     console.log('error inside onSlidingStart callback', error);
+        //   }
+        // }}
+        onSlidingComplete={async (value) => {
+          await sound.setPositionAsync(value * duration);
+          setPosition(value * duration);
+        }}
+      />
       <Button style={styles.button} onPress={() => pausePlayAudio()}>
         <Text>Play</Text>
       </Button>
