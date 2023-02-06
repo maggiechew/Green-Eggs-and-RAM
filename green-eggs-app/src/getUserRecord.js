ecf123399bec31635ba3ed8ad4c4f5a06758f6e8
yanliu1111 <liuyan0504@gmail.com>
2023-02-05 17:30:08 -0700

    database change from ref to array

--- green-eggs-app/src/getUserRecord.js
+++ green-eggs-app/src/getUserRecord.js
@@ -3,15 +3,19 @@ import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
 
 export const getIndividualUserRecord = async (id) => {
   const querySnapshot = await getDoc(doc(db, 'users', id));
-  const datanew = querySnapshot.data()?.eggs;
+  const eggdata = querySnapshot.data()?.eggs;
+  // const data = object.keys(datanew);
   console.log('querySnapshot', querySnapshot.data());
-  console.log('datanew', datanew);
+  console.log('eggdata', eggdata);
+  // console.log('data#####: ', data);
+
   if (querySnapshot.exists()) {
     console.log('Document data:', querySnapshot.data());
   } else {
     // doc.data() will be undefined in this case
     console.log('No such document!');
   }
+  return querySnapshot.data();
 };
 
 //firebase.firestore().collection('sample').where('uid', '=', firebase.auth().currentUser.uid).get().then((querySnapshot) => { querySnapshot.forEach((doc) => { console.log(doc.id, ' => ', doc.data()); }); });
