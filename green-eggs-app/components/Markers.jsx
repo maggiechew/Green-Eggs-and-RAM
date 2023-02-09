8e9b0255a181286dc19e5eca61fa6f2ebcd8d257
Maggie Chew <105380332+maggiechew@users.noreply.github.com>
2023-02-08 23:00:06 -0700

    Function for new egg; adds eggID to user doc

--- green-eggs-app/components/Markers.jsx
+++ green-eggs-app/components/Markers.jsx
@@ -1,12 +1,23 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 import { Marker } from 'react-native-maps';
+import { doc, updateDoc, arrayUnion } from "firebase/firestore";
 import { AuthenticatedUserContext } from '../providers';
+import { auth, db } from '../config';
+
+const { userInfo, user } = React.useContext(AuthenticatedUserContext);
+const userEggs = userInfo.eggs;
+const userID = user.uid;
+
+const newContent = async (eggID) => {
+const newEggArray = userEggs.push(eggID)
+  await updateDoc(doc(db, 'users', userID), {eggs: arrayUnion(eggID)})
+  userInfo.eggs = newEggArray;
+  //TODO: modal with newContent helper in it
+}
 
 
 export const Markers = ({ zoneEggs, eggsInRange, navigation }) => {
-  const { userInfo } = React.useContext(AuthenticatedUserContext);
-  const userEggs = userInfo.eggs;
   return zoneEggs?.map((egg) => {
     let locked = true;
     let discovered = false;
@@ -20,9 +31,11 @@ export const Markers = ({ zoneEggs, eggsInRange, navigation }) => {
           longitude: egg.geopoint.longitude
         }}
         pinColor={locked ? 'red' : discovered? 'green' : 'yellow' }
-        onPress={(e) =>
-          locked ? console.log('too bad') : discovered? console.log('You already found me'): navigation.navigate('Content')
+        onPress={() =>
+          locked ? console.log('too bad') : discovered? navigation.navigate('Content'): newContent(egg.id)
         }
+
+        // TODO: modal with stillUnlocked loaded when egg pressed but not in range (or that can be moved to this component if need be)
       />
     );
   });
