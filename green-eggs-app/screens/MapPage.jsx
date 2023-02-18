0ed9be538ab79bdbc216aed71768cde1f44bbd4c
Vince Iannelli <viannelli@gmail.com>
2023-02-18 10:50:38 -0700

    added map style

--- green-eggs-app/screens/MapPage.jsx
+++ green-eggs-app/screens/MapPage.jsx
@@ -32,6 +32,222 @@ export const MapPage = ({ navigation, children }) => {
   const { userInfo, user } = authContext;
   const userID = user.uid;
 
+  const mapStyle = [
+    {
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#ebe3cd'
+        }
+      ]
+    },
+    {
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#523735'
+        }
+      ]
+    },
+    {
+      elementType: 'labels.text.stroke',
+      stylers: [
+        {
+          color: '#f5f1e6'
+        }
+      ]
+    },
+    {
+      featureType: 'administrative',
+      elementType: 'geometry.stroke',
+      stylers: [
+        {
+          color: '#c9b2a6'
+        }
+      ]
+    },
+    {
+      featureType: 'administrative.land_parcel',
+      elementType: 'geometry.stroke',
+      stylers: [
+        {
+          color: '#dcd2be'
+        }
+      ]
+    },
+    {
+      featureType: 'administrative.land_parcel',
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#ae9e90'
+        }
+      ]
+    },
+    {
+      featureType: 'landscape.natural',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#dfd2ae'
+        }
+      ]
+    },
+    {
+      featureType: 'poi',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#dfd2ae'
+        }
+      ]
+    },
+    {
+      featureType: 'poi',
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#93817c'
+        }
+      ]
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'geometry.fill',
+      stylers: [
+        {
+          color: '#a5b076'
+        }
+      ]
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#447530'
+        }
+      ]
+    },
+    {
+      featureType: 'road',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#f5f1e6'
+        }
+      ]
+    },
+    {
+      featureType: 'road.arterial',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#fdfcf8'
+        }
+      ]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#f8c967'
+        }
+      ]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'geometry.stroke',
+      stylers: [
+        {
+          color: '#e9bc62'
+        }
+      ]
+    },
+    {
+      featureType: 'road.highway.controlled_access',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#e98d58'
+        }
+      ]
+    },
+    {
+      featureType: 'road.highway.controlled_access',
+      elementType: 'geometry.stroke',
+      stylers: [
+        {
+          color: '#db8555'
+        }
+      ]
+    },
+    {
+      featureType: 'road.local',
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#806b63'
+        }
+      ]
+    },
+    {
+      featureType: 'transit.line',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#dfd2ae'
+        }
+      ]
+    },
+    {
+      featureType: 'transit.line',
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#8f7d77'
+        }
+      ]
+    },
+    {
+      featureType: 'transit.line',
+      elementType: 'labels.text.stroke',
+      stylers: [
+        {
+          color: '#ebe3cd'
+        }
+      ]
+    },
+    {
+      featureType: 'transit.station',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#dfd2ae'
+        }
+      ]
+    },
+    {
+      featureType: 'water',
+      elementType: 'geometry.fill',
+      stylers: [
+        {
+          color: '#b9d3c2'
+        }
+      ]
+    },
+    {
+      featureType: 'water',
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#92998d'
+        }
+      ]
+    }
+  ];
+
   const defaultPicture = require('../assets/defaultavatar.jpg');
 
   async function _getEggTotal() {
@@ -130,10 +346,9 @@ export const MapPage = ({ navigation, children }) => {
   }, [arrayOfZones]);
 
   useEffect(() => {
-
     if (activeZone) {
       _getEggTotal();
-      setShowMenu(false)
+      setShowMenu(false);
       setShowModal(true);
     } else {
       setShowModal(false);
@@ -184,7 +399,7 @@ export const MapPage = ({ navigation, children }) => {
   useEffect(() => {
     const discoveredEggs = userInfo?.discoveredEggs;
     const allDiscoveredEggs = discoveredEggs?.length || 0;
-    
+
     const totalEggs = userStats?.everyEgg;
     let returnValue = 0;
     const zoneEggLength = zoneEggs?.length;
@@ -192,10 +407,19 @@ export const MapPage = ({ navigation, children }) => {
       if (discoveredEggs?.find((discovered) => discovered == zoneEgg.id))
         returnValue++;
     });
-    const percentageZoneDiscovered = Math.ceil((returnValue / zoneEggLength) * 100);
-    const percentageAllDiscovered = Math.ceil((allDiscoveredEggs/totalEggs) *100)
+    const percentageZoneDiscovered = Math.ceil(
+      (returnValue / zoneEggLength) * 100
+    );
+    const percentageAllDiscovered = Math.ceil(
+      (allDiscoveredEggs / totalEggs) * 100
+    );
 
-    setUserStats({ ...userStats, zoneFoundPercentage: percentageZoneDiscovered, allDiscovered: allDiscoveredEggs, allFoundPercentage: percentageAllDiscovered });
+    setUserStats({
+      ...userStats,
+      zoneFoundPercentage: percentageZoneDiscovered,
+      allDiscovered: allDiscoveredEggs,
+      allFoundPercentage: percentageAllDiscovered
+    });
   }, [zoneEggs, userInfo]);
 
   if (arrayOfZones == null) {
@@ -208,6 +432,7 @@ export const MapPage = ({ navigation, children }) => {
         <MapView
           style={styles.map}
           showsCompass={false}
+          customMapStyle={mapStyle}
           showsUserLocation
           showsMyLocationButton
           provider='google'
@@ -240,7 +465,7 @@ export const MapPage = ({ navigation, children }) => {
           }}
         >
           <Avatar.Image
-            style={[styles.avatar, { backgroundColor: 'black' }]}
+            style={[styles.avatar, { backgroundColor: `#111111` }]}
             source={
               userInfo?.avataruri == null
                 ? defaultPicture
@@ -285,8 +510,10 @@ const styles = StyleSheet.create({
   },
   avatar: {
     borderWidth: 3,
+    margin: 2,
     borderColor: 'gold',
-    padding: -3,
+    alignItems: 'center',
+    justifyContent: 'center',
     overflow: 'hidden'
   },
 
